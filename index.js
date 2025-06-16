a6bba1f10306ac032ffbdf313a1c319236f320f5
Павел Кроворов <creatorofworldsv2@gmail.com>
2025-06-16 23:19:20 +0300

    Versia 0.0.2

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Sofia memory plugin is running!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+// Placeholder for index.js
