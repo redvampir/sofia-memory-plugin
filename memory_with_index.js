75b633094b1245a4e331bb95731737b73c17359d
redvampir <93953323+redvampir@users.noreply.github.com>
2025-06-24 16:14:26 +0300

    fix memory_with_index export

--- memory_with_index.js
+++ memory_with_index.js
@@ -1,5 +1,5 @@
 // Экспорт функции сохранения с обновлением индекса
-const { saveMemoryWithIndex: save_memory_with_index } = require('./logic/storage');
+const { save_memory_with_index } = require('./logic/storage');
 
 module.exports = {
   // Функция сохраняет файл и обновляет индекс
