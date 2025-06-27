6b3a2935b8b51c1c9343effc00c1b7f0e59efa1d
redvampir <93953323+redvampir@users.noreply.github.com>
2025-06-27 17:05:51 +0300

    Remove unused memory_with_index module

--- memory_with_index.js
+++
@@ -1,7 +0,0 @@
-// Экспорт функции сохранения с обновлением индекса
-const { save_memory_with_index } = require('./logic/storage');
-
-module.exports = {
-  // Функция сохраняет файл и обновляет индекс
-  save_memory_with_index,
-};
